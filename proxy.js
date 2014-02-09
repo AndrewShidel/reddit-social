3f0d7e861d095cd0a7336430c8064e4cb02b33e0
Shidel-dev <andrew@shidel.com>
2014-02-08 23:06:19 -0500

    Added ability to retrieve and render comments from any reddit comments url.

--- proxy.js
+++ proxy.js
@@ -10,7 +10,7 @@ console.log('Listening on port 3000');
 
 app.use(express.static(__dirname + '/public'));
  
-app.get('*', function(req, res) {
+app.get('/post/', function(req, res) {
     //A function that takes a url and downloads the source. This will be defined in the next step.
     //console.log("Get: " + req.params[0].substring(0));
 	console.log("URl: "+req.query.url+"\nJSON: "+req.query.json);
@@ -18,7 +18,25 @@ app.get('*', function(req, res) {
 	getSource(req.query.url, req.query.json, res);
 });
 
+app.get('/get/', function(req, res) {
+    //A function that takes a url and downloads the source. This will be defined in the next step.
+    //console.log("Get: " + req.params[0].substring(0));
+	var url = "http://" + req.query.url;
+	console.log("Get: " + url);
+	//res.send("Done");
+	getSource2(url, res);
+});
+function getSource2(uri, res){
+	request(uri, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(body);                
+        }else{
+            res.send("An error Has occured, the url was most likly incorrect.");
+        }
+    });
+}
 function getSource(uri, json, res) {
+	//uri = decodeURIComponent(uri);
     //Get the source of the url.
     request.post(uri, json, function(error, response, body) {
         if (!error && response.statusCode == 200) {
